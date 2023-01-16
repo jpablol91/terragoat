12b2d84df771fdc3ad4fa6cf47ffdc7a504595fd
jpablol91 <jpablol91@gmail.com>
2023-01-15 21:12:31 -0500

    Update simple_instance/s3.tf
    Co-authored-by: bridgecrew[bot] <60663194+bridgecrew[bot]@users.noreply.github.com>

--- simple_instance/s3.tf
+++ simple_instance/s3.tf
@@ -18,3 +18,51 @@ resource "aws_s3_bucket" "dockingbay" {
     yor_trace            = "20ce17b3-eddb-40d6-ae01-db27427d0f03"
   }
 }
+
+
+resource "aws_s3_bucket_versioning" "dockingbay" {
+  bucket = aws_s3_bucket.dockingbay.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket" "destination" {
+  bucket = aws_s3_bucket.dockingbay.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_iam_role" "replication" {
+  name = "aws-iam-role"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "s3.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket_replication_configuration" "dockingbay" {
+  depends_on = [aws_s3_bucket_versioning.dockingbay]
+  role   = aws_iam_role.dockingbay.arn
+  bucket = aws_s3_bucket.dockingbay.id
+  rule {
+    id = "foobar"
+    status = "Enabled"
+    destination {
+      bucket        = aws_s3_bucket.destination.arn
+      storage_class = "STANDARD"
+    }
+  }
+}
